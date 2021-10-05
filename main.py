d3dd08a9f667809a07ab0b33b1119defe3a51902
Mahesh1414 <89467357+Mahesh1414@users.noreply.github.com>
2021-10-05 19:34:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def subject(client,message):
     await message.reply_text(
         text=f"Hey! Choose the Subject",
         disable_web_page_preview=True,
-        reply_makeup=InlineKeyboardMarkup(
+        reply_makup=InlineKeyboardMarkup(
                    [[
                       InlineKeyboardButton(
                                  text="Biology",
