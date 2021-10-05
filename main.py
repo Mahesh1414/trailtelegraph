9738fce5aaf2cbbbe486f97f846dabc5168fe904
Mahesh1414 <89467357+Mahesh1414@users.noreply.github.com>
2021-10-05 22:57:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def chemistry (client.message):
         reply_markup=InlineKeyboardMarkup(CCHAPTER_BTN)
       )
     
-   CCHAPTER_BTN =[[
+   CCHAPTER_BTN = [[
        InlineKeyboardButoon('Chapter 01', url=f'https://telegram.me/bryllbots')
        ],[
        InlineKeyboardButton('Chapter 02', url=f'https://telegram.me/beyllbots')
