be5da6fd2fdc4703a3f5837031f5f04a9cdbce7b
justxd22 <xdjust18@gmail.com>
2021-11-03 12:31:16 +0300

    fix

--- Config.py
+++ Config.py
@@ -4,5 +4,6 @@ class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
     API_ID = int(os.getenv("API_ID", ""))
     API_HASH = os.getenv("API_HASH", "")
-    FORCE_SUB1 = os.environ.get("FORCE_SUB", "bryllbots") if os.environ.get("FORCE_SUB") else print("please add the group/channel name in env in heroku")
-    FORCE_SUB2 = os.environ.get("FORCE_SUB", "tttt10101001") if os.environ.get("FORCE_SUB") else print("please add the group/channel name in env in heroku")
+    FORCE_SUB1 = os.environ.get("FORCE_SUB1", "bryllbots") if os.environ.get("FORCE_SUB") else print("please add the group/channel name in env in heroku")
+    FORCE_SUB2 = os.environ.get("FORCE_SUB2", "bryll_education") if os.environ.get("FORCE_SUB") else print("please add the group/channel name in env in heroku")
+
